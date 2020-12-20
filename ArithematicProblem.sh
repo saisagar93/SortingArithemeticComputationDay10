4deba23c582d9638f0423db9aedca30a53777bbf
saisagar93 <sscreator225@gmail.com>
2020-12-20 12:42:06 +0530

    Add :code Compute a

--- ArithematicProblem.sh
+++ ArithematicProblem.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+echo -ne "\n"
+
 read -p "Enter First Number : " a
 read -p "Enter Second Number : " b
 read -p "Enter Third Number : " c
 
-echo "The Taken Inputs Are $a $b $c"
+echo "The Numbers Are $a $b $c"
+
+p=$(( a+(b*c) ))
+
+echo "The Result is" $p
+
